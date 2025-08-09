a8c0f46f15627f2cbf47e3f0e2d180ad1502e7f8
tuan2000 <quoctuan2000bg@gmail.com>
2025-08-09 08:55:25 +0700

    Completed VMTranslator

--- Projects/08/VMTranslator/VMTranslator.cpp
+++ Projects/08/VMTranslator/VMTranslator.cpp
@@ -7,12 +7,9 @@ using namespace std;
 
 namespace fs = std::filesystem;
 
-int Translate(const string& fileName){
+void Translate(CodeWriter& codeWriter, const string& fileName){
     Parser parser(fileName);
-    fs::path filePath = fileName;
-    filePath.replace_extension(".asm");
-    CodeWriter codeWriter(filePath.string(), fileName);
-
+    codeWriter.setFileName(fileName);
     while(parser.hasMoreCommands()){
         parser.advance();
         switch (parser.commandType())
@@ -28,32 +25,81 @@ int Translate(const string& fileName){
             codeWriter.writePushPop(parser.commandType(), parser.arg1(), parser.arg2());
             break;
         }
+        case VM_COMMAND_TYPE::C_LABEL:
+        {
+            codeWriter.writeLabel(parser.arg1());
+            break;
+        }
+        case VM_COMMAND_TYPE::C_GOTO:
+        {
+            codeWriter.writeGoto(parser.arg1());
+            break;
+        }
+        case VM_COMMAND_TYPE::C_IF:{
+            codeWriter.writeIf(parser.arg1());
+            break;
+        }
+        case VM_COMMAND_TYPE::C_FUNCTION:
+        {
+            codeWriter.writeFuncion(parser.arg1(), parser.arg2());
+            break;
+        }
+        case VM_COMMAND_TYPE::C_CALL:
+        {
+            codeWriter.writeCall(parser.arg1(), parser.arg2());
+            break;
+        }
+        case VM_COMMAND_TYPE::C_RETURN:
+        {
+            codeWriter.writeReturn();
+        }
         
         default:
             break;
         }
         
     }
-
-    codeWriter.close();
 }
 
 int main(int argc, char* argv[]){
-    if(argc < 3){
+    std::string outPutFile = argv[1];
+    std::string newExtension = ".asm";
+    bool noMainFunction = false;
+
+    if(argc < 2){
         cout << "Invalid argument!" << endl;
         return -1;
     }
 
-    std::string pathStr = argv[1];
-    fs::path path(pathStr);
+    if(argc == 3){
+        string arg2 = argv[2];
+        if(arg2.compare("-f") != 0){
+            cout << "Invalid argument!" << endl;
+            return -1;
+        }
 
+        noMainFunction = true;
+    }
+
+    // Find the position of the last dot
+    size_t lastDotPos = outPutFile.find_last_of('.');
+    if (lastDotPos != std::string::npos) {
+        // Replace the extension
+        outPutFile = outPutFile.substr(0, lastDotPos) + newExtension;
+    } else {
+        // If no extension exists, simply append the new one
+        outPutFile += newExtension;
+    }
+    
+    fs::path path(argv[1]);
+    CodeWriter codeWriter(outPutFile, noMainFunction);
     if (fs::exists(path)) {
         if (fs::is_regular_file(path)) {
-            Translate(path.string());
+            Translate(codeWriter, argv[1]);
         } else if (fs::is_directory(path)) {
             for (const auto& entry : fs::directory_iterator(path)) {
                 if (entry.is_regular_file() && entry.path().extension() == ".vm") {
-                    Translate(entry.path().string());
+                    Translate(codeWriter, entry.path().string());
                 }
             }
         } else {
@@ -64,7 +110,9 @@ int main(int argc, char* argv[]){
         std::cout << "Path is not valid (does not exist).\n";
     }
 
-    
+    codeWriter.close();
+
+    std::cout << "Successfule! Out put file is: " + outPutFile << endl; 
 
     return 0;
 }
---
+++ Projects/08/VMTranslator/readme.txt
@@ -0,0 +1,4 @@
+1. How to translate VM code that does not have main function (ie: SimpleFuntion)
+    VMTranslator.exe <file> -f 
+2. How to translate VM code that have main function (ie: FibonacciElement)
+    VMTranslator.exe <file or folder>
